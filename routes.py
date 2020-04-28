2c379f592b0dc9771394d668ec3b087b43ed71f1
Tori Wells <twells24@liberty.edu>
2020-04-27 22:16:29 -0400

    Setup inital page layout
    Add html page for home, timesheets, and milestones. All html pages
    inherit from a base html. Added a bootstrap css override.

--- routes.py
+++ routes.py
@@ -6,8 +6,18 @@ import os
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def index():
-  return render_template("base.html")
+  return render_template("home.html")
+
+@app.route('/timesheets')
+def timesheets():
+  return render_template("timesheets.html", title="timesheets")
+
+@app.route('/milestones')
+def milestones():
+  return render_template("milestones.html")
+
 
 if __name__ == '__main__':
 	app.secret_key = os.urandom(12)
